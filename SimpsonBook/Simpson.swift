954233449cb6bfd1267bd260d311c779946d4ee7
Kerem Türközü <turkozukerem@gmail.com>
2024-12-02 23:18:45 +0300

    SimpsonBook added

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,25 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Kerem Türközü on 28.08.2024.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name = String()
+    var job = String()
+    var image = UIImage()
+    
+    init(simpsonName: String, simpsonJob:String, simpsonImage:UIImage) {
+            name = simpsonName
+            job = simpsonJob
+            image = simpsonImage
+    }
+    
+    
+    
+}
